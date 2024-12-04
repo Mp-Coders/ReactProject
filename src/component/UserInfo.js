96ae55edbb53594bf1d3440f5390b8197c22426d
Mp-Coders <fatimamumtaz0055@gmail.com>
2024-12-04 14:53:35 +0500

    lab

--- src/component/UserInfo.js
+++ src/component/UserInfo.js
@@ -4,7 +4,7 @@ import "../styles/UserInfo.css";
 function UserInfo({ user }) {
   return (
     <div className="card user-info">
-      <img className="femalepic.jpg" src={user.profilePicture} alt="Profile" />
+      <img className="profile-picture" src="femalepic.jpg" alt="Profile" />
       <h2>{user.name}</h2>
       <p>{user.email}</p>
       <span className={`role-label ${user.role.toLowerCase()}`}>
